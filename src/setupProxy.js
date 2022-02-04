30cc77d52fad767bdbbca5d590beb6ed0d213279
Hafidz Azhar <hafidz@formulatrix.com>
2022-02-04 10:42:04 +0700

    fixed setup proxy configuration for web socket
    (cherry picked from commit 82e0a174703e989315921ddaa15917584d3e8325)

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,5 +2,5 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
   app.use('/api', createProxyMiddleware({ target: 'http://localhost:8000/' }));
-  app.use('/hub', createProxyMiddleware({ target: 'http://localhost:8000/', ws: true }));
+  app.use(createProxyMiddleware('/hub', { target: 'http://localhost:8000/', ws: true }));
 };
